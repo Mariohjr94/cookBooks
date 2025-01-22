b9a0adabf3f580e7678b1a9849d927e12f27ae42
Mariohjr94 <mariohjr94@gmail.com>
2025-01-21 23:38:42 -0500

    fixing bugs on register

--- CookBooks/server/auth/index.js
+++ CookBooks/server/auth/index.js
@@ -52,7 +52,7 @@ router.post("/login", async (req, res, next) => {
       rows: [admin],
     } = await db.query(
       "SELECT * FROM admin WHERE username = $1",
-      [req.body.username.toLowerCase()]
+      [req.body.username]
     );
 
     if (!admin) {
